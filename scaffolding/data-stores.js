12c42fd56146b73c065f1cacb44d71fc71ceca5f
SciutoAlex <SciutoAlex@gmail.com>
2016-01-29 11:05:22 -0500

    initial commit

---
+++ scaffolding/data-stores.js
@@ -0,0 +1,12 @@
+ var data = [
+    {"region" : "africa", "country":"South Africa","1960-age":49.0,"2013-age":56.7,"1960-gdp":3543.5,"2013-gdp":6090.3},
+    {"region" : "americas", "country":"Uruguay","1960-age":67.9,"2013-age":76.8,"1960-gdp":3152.0,"2013-gdp":7771.9},
+    {"region" : "asia", "country":"Malaysia","1960-age":59.5,"2013-age":74.6,"1960-gdp":986.5,"2013-gdp":7051.8},
+    {"region" : "europe", "country":"Luxembourg","1960-age":68.4,"2013-age":81.8,"1960-gdp":21613.6,"2013-gdp":81581.5},
+    {"region" : "europe", "country":"Italy","1960-age":69.1,"2013-age":82.3,"1960-gdp":9326.7,"2013-gdp":29101.0},
+    {"region" : "europe", "country":"Iceland","1960-age":73.4,"2013-age":83.1,"1960-gdp":15753.6,"2013-gdp":59018.2},
+    {"region" : "europe", "country":"Greece","1960-age":68.2,"2013-age":80.6,"1960-gdp":5520.1,"2013-gdp":18022.3},
+    {"region" : "asia", "country":"China","1960-age":43.5,"2013-age":75.4,"1960-gdp":121.2,"2013-gdp":3619.4},
+    {"region" : "americas", "country":"Bahamas, The","1960-age":62.7,"2013-age":75.1,"1960-gdp":13380.9,"2013-gdp":20736.5}
+];
+
